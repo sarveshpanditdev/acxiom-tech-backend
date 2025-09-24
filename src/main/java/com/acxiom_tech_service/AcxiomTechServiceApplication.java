74e99173ee7ddca7a34155c2113bccc1bfe8812e
sarveshpanditdev <sarveshpandit.dev@gmail.com>
2025-09-24 19:01:31 +0530

    Backend service new created

---
+++ src/main/java/com/acxiom_tech_service/AcxiomTechServiceApplication.java
@@ -0,0 +1,13 @@
+package com.acxiom_tech_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcxiomTechServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcxiomTechServiceApplication.class, args);
+	}
+
+}
